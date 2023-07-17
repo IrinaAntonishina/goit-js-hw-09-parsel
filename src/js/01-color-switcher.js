c476de7cc26b49ed9fe503308826af3389cb02b4
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-07-17 20:02:16 +0300

    task 1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,33 @@
+
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body')
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+let btnActive = false;
+let interval = null
+  refs.btnStart.addEventListener('click', (evt) => {
+    if(btnActive){
+        return;
+    }
+    interval = setInterval(() => {
+        btnActive = true;
+        const colorBody = getRandomHexColor();
+        refs.body.style.backgroundColor = `${colorBody}`;
+        
+    }, 1000)
+    
+})
+
+const stop = refs.btnStop.addEventListener('click',() => {
+clearInterval(interval);
+if(btnActive){
+    btnActive = false;
+}
+
+})
