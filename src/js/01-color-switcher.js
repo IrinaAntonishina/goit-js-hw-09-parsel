5867441a5e012d9203ff03dab1f7bc8ec0ff55d7
Irina Antonishina <111454571+IrinaAntonishina@users.noreply.github.com>
2023-07-22 17:49:25 +0300

    change task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -13,10 +13,14 @@ let btnActive = false;
 let interval = null
   refs.btnStart.addEventListener('click', (evt) => {
     if(btnActive){
+        
         return;
     }
     interval = setInterval(() => {
         btnActive = true;
+        if(btnActive){
+            refs.btnStart.setAttribute('disabled', true)
+        }
         const colorBody = getRandomHexColor();
         refs.body.style.backgroundColor = `${colorBody}`;
         
@@ -29,5 +33,7 @@ clearInterval(interval);
 if(btnActive){
     btnActive = false;
 }
-
+if(!btnActive){
+    refs.btnStart.removeAttribute('disabled')
+}
 })
